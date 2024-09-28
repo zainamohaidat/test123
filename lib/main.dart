be9f403773c6d61d60aeb2951ef1d02cc616b25d
zainamohaidat <zainamohidat@gmail.com>
2024-09-29 01:19:13 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -13,34 +13,114 @@ class MyApp extends StatelessWidget{
      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("App bar ",
+          title: Text("Product List ",
           style: TextStyle(
             color: Colors.white,
             fontSize: 30,
             fontWeight: FontWeight.bold
           ),
           ),
-          backgroundColor: Colors.deepPurpleAccent,
+          backgroundColor: Colors.green,
         ),
         body:Column(
           children: [
-            Text("Result $count",
-            style:TextStyle(
-              fontSize: 30,
-              
+            Container(
+              child: Row(
+                children: [
+                  ConstrainedBox(
+                    child: Image.asset("assets/images/iphone.jpg",height: 1000,),
+                    constraints: BoxConstraints(
+                      maxHeight: 130,
+                      maxWidth: 150,
+                    ),
+                  ),
+                  SizedBox(width: 200,),
+                  Column(
+                    children: [
+                      Text("iphone"),
+                      Text("Apple iPhone 14 Pro Max"),
+                      Text("price:1000 JD")
 
-              ),)
-            ,Checkbox(value: false,
-                onChanged:(x) {
-              count++;
-            print("count ===> $count");
-            }
-            )
+                    ],
+                  ),
 
-          ],
-        )
+                ],
+              ),
+            ),
+            Container(
+            child: Row(
+            children: [
+                 ConstrainedBox(
+                 child: Image.network("https://d1b5h9psu9yexj.cloudfront.net/60866/Samsung-Galaxy-S24-Ultra_20240319-181821_full.jpeg"),
+                  constraints: BoxConstraints(
+                   maxHeight: 130,
+                  maxWidth: 150,
+    ),
+    ),
+            SizedBox(width: 200,),
+            Column(
+             children: [
+               Text("Android"),
+                Text("Samsung Galaxy Ultra S23"),
+                Text("price:900 JD")
 
-      ),
+    ],
+    ),
+
+    ],
+    ),
+    ),
+            Container(
+              child: Row(
+                children: [
+                  ConstrainedBox(
+                    child: Image.asset("assets/images/labtop.jpg",height: 1000,),
+                    constraints: BoxConstraints(
+                      maxHeight: 130,
+                      maxWidth: 150,
+                    ),
+                  ),
+                  SizedBox(width: 200,),
+                  Column(
+                    children: [
+                      Text("Laptop"),
+                      Text("Apple Macbook Laptop"),
+                      Text("price:1000 JD")
+
+                    ],
+                  ),
+
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                children: [
+                  ConstrainedBox(
+                    child: Image.network("https://www.apple.com/newsroom/images/product/os/ipados/Apple_iPadOS_Today-View_060319_big.jpg.large.jpg",height: 1000,),
+                    constraints: BoxConstraints(
+                      maxHeight: 130,
+                      maxWidth: 150,
+                    ),
+                  ),
+                  SizedBox(width: 200,),
+                  Column(
+                    children: [
+                      Text("iPad"),
+                      Text("iPadOS 18 Apple"),
+                      Text("price:700 JD")
+
+                    ],
+                  ),
+
+                ],
+              ),
+            ),
+        ],),
+      )
+          
+
+
    );
   }
 }
